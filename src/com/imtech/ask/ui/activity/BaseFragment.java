c8b0a954d8a0ff080d48a765486d7af4f0438835
dreaner <dreaner@dreaner-F8Va>
2013-06-07 00:35:02 +0800

    1.增加下拉刷新；

---
+++ src/com/imtech/ask/ui/activity/BaseFragment.java
@@ -0,0 +1,7 @@
+package com.imtech.ask.ui.activity;
+
+import android.support.v4.app.Fragment;
+
+public class BaseFragment extends Fragment{
+
+}
---
+++ src/com/imtech/ask/ui/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.imtech.ask.ui.activity;
+
+import com.imtech.ask.R;
+import com.imtech.ask.R.layout;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
