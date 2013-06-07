5959f36f7b9ad1d62d211cfaec1d2b785fb7cc9d
douzifly <douzifly@gmail.com>
2013-06-07 14:42:29 +0800

    remove some log

--- src/com/imtech/ask/ui/BaseFragment.java
+++ src/com/imtech/ask/ui/BaseFragment.java
@@ -8,7 +8,6 @@ package com.imtech.ask.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,37 +23,37 @@ public class BaseFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		Log.d(TAG, "onCreateView " + this);
+//		Log.d(TAG, "onCreateView " + this);
 		return super.onCreateView(inflater, container, savedInstanceState);
 	}
 	
 	@Override
 	public void onDestroyView() {
-		Log.d(TAG, "onDestroyView " + this);
+//		Log.d(TAG, "onDestroyView " + this);
 		super.onDestroyView();
 	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		Log.d(TAG, "onCreate " + this);
+//		Log.d(TAG, "onCreate " + this);
 		super.onCreate(savedInstanceState);
 	}
 	
 	@Override
 	public void onDestroy() {
-		Log.d(TAG, "onDestroy " + this);
+//		Log.d(TAG, "onDestroy " + this);
 		super.onDestroy();
 	}
 	
 	@Override
 	public void onDetach() {
-		Log.d(TAG, "onDetach " + this);
+//		Log.d(TAG, "onDetach " + this);
 		super.onDetach();
 	}
 	
 	@Override
 	public void onAttach(Activity activity) {
-		Log.d(TAG, "onAttach " + this);
+//		Log.d(TAG, "onAttach " + this);
 		super.onAttach(activity);
 	}
 }
