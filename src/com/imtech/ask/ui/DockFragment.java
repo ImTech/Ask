69adfa099caafe592688d89039e087bdcd2831fa
douzifly <douzifly@gmail.com>
2013-06-11 12:52:29 +0800

    topbar

--- src/com/imtech/ask/ui/DockFragment.java
+++ src/com/imtech/ask/ui/DockFragment.java
@@ -5,17 +5,15 @@
  */
 package com.imtech.ask.ui;
 
-import com.imtech.ask.R;
-import com.imtech.ask.ui.IDockFragment.DockListener;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
+
+import com.imtech.ask.R;
 
 /**
  * @author douzifly
---
+++ src/com/imtech/ask/view/TopBar.java
@@ -0,0 +1,30 @@
+/*
+ * @project : Ask
+ * @author  : huqiming 
+ * @date    : 2013-6-11
+ */
+package com.imtech.ask.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Application top bar
+ */
+public class TopBar extends FrameLayout{
+
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public TopBar(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initView(context);
+	}
+	
+	private void initView(Context context){
+	}
+	
+	
+}
